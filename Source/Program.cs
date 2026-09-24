7c8605700d13190b92b35bae5bb3701ce6d1b2c0
Gamba <juanca.quinterom@gmail.com>
2026-09-24 03:10:11 -0500

    Kill previous instances on launch
    After a successful initialization, previously existing instances are terminated to avoid duplicates

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.IO;
 using System.Management;
 using System.Security.Principal;
@@ -14,35 +16,16 @@ public static class Program
 
 	private static void Main()
 	{
-		if (TryInit(out string condition))
+		if (TryInit())
 		{
-			Watch("Win32_ProcessStartTrace", condition, OnProcessStart);
-			Watch("Win32_ProcessStopTrace", condition, OnProcessStop);
-
-			StTrinaSleep();
+			WatchEvents();
+			FinishThread();
 		}
 	}
 
 	#region Init
 
-	private static bool TryInit(out string condition)
-	{
-		condition = null;
-
-		if (IsAdministrator() && TryInitData())
-		{
-			List<string> conditions = new List<string>(data.Count);
-
-			foreach (string key in data.Keys)
-			{
-				conditions.Add($"ProcessName like '{key.Truncate(14)}%'");
-			}
-
-			condition = string.Join(" or ", conditions);
-		}
-
-		return condition != null;
-	}
+	private static bool TryInit() => IsAdministrator() && TryParseData();
 
 	private static bool IsAdministrator()
 	{
@@ -52,7 +35,7 @@ public static class Program
 		return principal.IsInRole(WindowsBuiltInRole.Administrator);
 	}
 
-	private static bool TryInitData()
+	private static bool TryParseData()
 	{
 		if (File.Exists(config))
 		{
@@ -65,8 +48,11 @@ public static class Program
 					string process = contents[0].Trim();
 					string program = contents[1].Trim();
 
-					data.Add(process, program);
-					states.Add(process, false);
+					if (ValidateContents(process, program))
+					{
+						data.Add(process, program);
+						states.Add(process, false);
+					}
 				}
 			}
 		}
@@ -74,9 +60,9 @@ public static class Program
 		return data.Count > 0;
 	}
 
-	private static string Truncate(this string value, int length)
+	private static bool ValidateContents(params string[] contents)
 	{
-		return value.Length > length ? value.Substring(0, length) : value;
+		return !contents.Any(content => content == "");
 	}
 
 	#endregion
@@ -85,6 +71,31 @@ public static class Program
 
 	#region Watch
 
+	private static void WatchEvents()
+	{
+		string condition = GetCondition();
+
+		Watch("Win32_ProcessStartTrace", condition, OnProcessStart);
+		Watch("Win32_ProcessStopTrace", condition, OnProcessStop);
+	}
+
+	private static string GetCondition()
+	{
+		List<string> conditions = new List<string>(data.Count);
+
+		foreach (string key in data.Keys)
+		{
+			conditions.Add($"ProcessName like '{key.Truncate(14)}%'");
+		}
+
+		return string.Join(" or ", conditions);
+	}
+
+	private static string Truncate(this string value, int length)
+	{
+		return value.Length > length ? value.Substring(0, length) : value;
+	}
+
 	private static void Watch(string className, string condition, EventArrivedEventHandler handler)
 	{
 		WqlEventQuery query = new WqlEventQuery(className, condition);
@@ -92,6 +103,50 @@ public static class Program
 
 		watcher.EventArrived += handler;
 		watcher.Start();
+	}
+
+	#endregion
+
+	// ----------------------------------------------------------------------------------------------------
+
+	#region Finish
+
+	private static void FinishThread()
+	{
+		KillInstances();
+		StTrinaSleep();
+	}
+
+	private static void KillInstances()
+	{
+		Process process = Process.GetCurrentProcess();
+		Process[] instances = Process.GetProcessesByName(process.ProcessName);
+
+		foreach (Process instance in instances)
+		{
+			if (instance.Id != process.Id)
+			{
+				KillProcessTree(instance.Id);
+			}
+		}
+	}
+
+	private static void KillProcessTree(int id)
+	{
+		SelectQuery query = new SelectQuery("Win32_Process", $"ParentProcessID = {id}");
+		ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+		foreach (ManagementBaseObject obj in searcher.Get())
+		{
+			KillProcessTree(obj.GetProcessID());
+		}
+
+		Process.GetProcessById(id).Kill();
+	}
+
+	private static int GetProcessID(this ManagementBaseObject obj)
+	{
+		return Convert.ToInt32(obj["ProcessID"]);
 	}
 
 	private static void StTrinaSleep()
@@ -107,7 +162,7 @@ public static class Program
 
 	private static void OnProcessStart(object sender, EventArrivedEventArgs eventArgs)
 	{
-		string process = GetProcess(eventArgs);
+		string process = eventArgs.GetProcessName();
 
 		if (states.TryGetValue(process, out bool isRunning) && !isRunning)
 		{
@@ -119,7 +174,7 @@ public static class Program
 
 	private static void OnProcessStop(object sender, EventArrivedEventArgs eventArgs)
 	{
-		string truncatedProcess = GetProcess(eventArgs);
+		string truncatedProcess = GetProcessName(eventArgs);
 
 		foreach (string process in states.Keys)
 		{
@@ -130,9 +185,9 @@ public static class Program
 		}
 	}
 
-	private static string GetProcess(EventArrivedEventArgs eventArgs)
+	private static string GetProcessName(this EventArrivedEventArgs eventArgs)
 	{
-		return (string)eventArgs.NewEvent.Properties["ProcessName"].Value;
+		return (string)eventArgs.NewEvent["ProcessName"];
 	}
 
 	private static bool Exists(string process)
